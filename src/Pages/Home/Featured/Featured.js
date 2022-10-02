68ad371c40a2c60e6e8aa7f6a01298d8032f6711
ayush-lab <ayush.millionaire8750@gmail.com>
2022-10-02 22:57:23 +0530

    added annimation

--- src/Pages/Home/Featured/Featured.js
+++ src/Pages/Home/Featured/Featured.js
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import React from 'react';
 import Card from '../../../Components/FeaturedCard/Card';
 import styles from './Featured.module.css';
 import Card1 from '../../../Assets/Images/dev.gif';
@@ -7,19 +7,23 @@ import Birdminton from '../../../Assets/Images/birdminton.png'
 
 import Trello from '../../../Assets/Images/trello.png'
 import Gollaa from '../../../Assets/Images/golaa.png'
-import Repairo from '../../../Assets/Images/repairoo.png'
-import Answer from '../../../Assets/Images/answer.svg'
-
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Featured() {
     
+  
+  React.useEffect(() => {
+    Aos.init({ duration: 600 });
+  }, []);
+
+
   return (
      <div className={styles.Featured}>
 
          {/* <div className={styles.lineBreak}></div> */}
 
-         <h3>Explorations</h3>
+         <h3 data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-duration="1500">Explorations</h3>
 
          <div className={styles.projects}>
                <Card name={"Saheli"} image={Saheli} alt="pro" position="Lead react developer" />
--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -1,18 +1,26 @@
-import { React, useEffect } from "react";
+import React, { useEffect } from "react";
 import styles from "./Footer.module.css";
 import Github from "../../../Assets/Images/github.svg";
 import LinkedIn from "../../../Assets/Images/linkedin.svg";
 import Instagram from "../../../Assets/Images/instagram.svg";
 import { Button } from "antd";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Footer() {
+
+   React.useEffect(() => {
+      Aos.init({ duration: 600 });
+    }, []);
+  
+  
   return (
     <>
       <div className={styles.footer}>
         <div className={styles.footerSection}>
-          <h4 className={styles.heading}>Visit my profiles! </h4>
+          <h4 className={styles.heading} data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-duration="1500"  >Visit my profiles! </h4>
 
-          <div className={styles.Links}>
+          <div data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-duration="1500"  className={styles.Links}>
             <div>
               <img src={Github} alt="Github" />
             </div>
@@ -38,7 +46,7 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className={styles.mailBtn}>
+          <div data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-duration="1500"  className={styles.mailBtn}>
             <Button primary >
               <a href="mailto:ayush1911052@akgec.ac.in">
                 Mail me at: <b>ayush1911052@akgec.ac.in</b>
